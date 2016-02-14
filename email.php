0753df0bb8a71eec1211195c5c45183ae948265d
AmyAmyZhu <zhuzefang12@gmail.com>
2016-02-13 23:21:52 -0500

    test

--- email.php
+++ email.php
@@ -1,5 +1,20 @@
-<?php 
-       mail("zhuzefang12@gmail.com", "haha", " ", "hehe");
-<?php
-}
+        <?php
+            $to = "zhuzefang12@gmail.com";
+            $subject = "This is subject";
+            
+            $message = "<b>This is HTML message.</b>";
+            $message .= "<h1>This is headline.</h1>";
+            
+            $header = "From:roy.c.x@outlook.com \r\n";
+            $header = "Cc:roy.c.x@outlook.com \r\n";
+            $header .= "MIME-Version: 1.0\r\n";
+            $header .= "Content-type: text/html\r\n";
+            
+            $retval = mail ($to,$subject,$message,$header);
+            
+            if( $retval == true ) {
+                echo "Message sent successfully...";
+            }else {
+                echo "Message could not be sent...";
+            }
 ?>
