7c429e080c79443eaf77b05a2c616eeb8b7ef581
AmyAmyZhu <zhuzefang12@gmail.com>
2016-02-13 23:15:50 -0500

    simple simple contact

--- email.php
+++ email.php
@@ -1,12 +1,5 @@
 <?php 
-
-       $email_from = "zhuzefang12@gmail.com";
-       $email_subject = "New Form submission";
-       $email_body = "hahahaha";
-       $to = "zhuzefang12@gmail.com";
-       $headers = "from: $email_from \r\n";
-       
-       mail($to, $email_subject, $email_body, $headers);
+       mail("zhuzefang12@gmail.com", "haha", " ", "hehe");
 <?php
 }
 ?>
