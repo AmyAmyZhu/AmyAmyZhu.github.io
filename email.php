1394649ff83d1b538ccc21560c15b8ccf379d7b9
AmyAmyZhu <zhuzefang12@gmail.com>
2016-02-13 23:12:54 -0500

    simple contact

--- email.php
+++ email.php
@@ -1,34 +1,12 @@
 <?php 
 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "zhuzefang12@gmail.com";
-    $email_subject = "Your email subject line";
-
-    $Name = $_POST['Name']; // required
-    $Email = $_POST['Email']; // required
-    $Message = $_POST['Message']; // required
- 
-    $email_message = "Form details below.\n\n";
-
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string); 
-    } 
- 
-    $email_message .= "Name: ".clean_string($Name)."\n";
-    $email_message .= "Email: ".clean_string($Email)."\n";
-    $email_message .= "Message: ".clean_string($Message)."\n";
- 
-// create email headers
-$headers = 'From: '.$Email."\r\n".
-'Reply-To: '.$Email."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $Email, $email_message, $headers);  
-?>
- 
-<!-- include your own success html here -->
-Thank you for contacting us. We will be in touch with you very soon.
-
+       $email_from = "zhuzefang12@gmail.com";
+       $email_subject = "New Form submission";
+       $email_body = "email address: $visitor_email\nHere is the message:\n $message";
+       $to = "zhuzefang12@gmail.com";
+       $headers = "from: $email_from \r\n";
+       
+       mail($to, $email_subject, $email_body, $headers);
 <?php
 }
 ?>
