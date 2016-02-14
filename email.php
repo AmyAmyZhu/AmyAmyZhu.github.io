44a0b08f366785958eb4ea3133e3bdba07d8afda
AmyAmyZhu <zhuzefang12@gmail.com>
2016-02-14 01:56:45 -0500

    preloader

--- email.php
+++ email.php
@@ -1,20 +1,10 @@
 <?php
-            $to = "zhuzefang12@gmail.com";
-            $subject = "This is subject";
-            
-            $message = "<b>This is HTML message.</b>";
-            $message .= "<h1>This is headline.</h1>";
-            
-            $header = "From:roy.c.x@outlook.com \r\n";
-            $header = "Cc:roy.c.x@outlook.com \r\n";
-            $header .= "MIME-Version: 1.0\r\n";
-            $header .= "Content-type: text/html\r\n";
-            
-            $retval = mail ($to,$subject,$message,$header);
-            
-            if( $retval == true ) {
-                echo "Message sent successfully...";
-            }else {
-                echo "Message could not be sent...";
-            }
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("zhuzefang12@gmail.com","My subject",$msg);
 ?>
